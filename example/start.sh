00c6f568faf6263ec3ce68d7b5828844d4741b31
Victor <3372598+victornpb@users.noreply.github.com>
2025-07-02 18:24:19 +0100

    Update start.sh

--- example/start.sh
+++ example/start.sh
@@ -4,8 +4,11 @@ cd "$( dirname "$0" )"
 # Keep the process running if it crashes
 while true; do
   echo "Updating proxy..."
-  npm i -g git@github.com:victornpb/HTTP-Reverse-Proxy.git
+  npm install -g https://github.com/victornpb/reproxy.git
+
+  echo "Starting proxy..."
   reverseproxy start
-  echo "Reverse Proxy crashed. Restarting in 5 seconds..."
+  
+  echo "Reverse Proxy stopped. Restarting in 5 seconds..."
   sleep 5
 done
